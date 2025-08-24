403940537d4dc60614d4fdb82752c8ebf5f942bc
RahulKMishraDev <rahulkumarmishra2003@gmail.com>
2025-08-24 20:35:56 +0530

    Initial commit

---
+++ inventoryServiceTestA/src/test/java/com/example/inventory/security/jwt/AuthenticationIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.example.inventory.security.jwt;
+
+import com.example.inventory.config.SecurityConfiguration;
+import com.example.inventory.config.SecurityJwtConfiguration;
+import com.example.inventory.config.WebConfigurer;
+import com.example.inventory.management.SecurityMetersService;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import tech.jhipster.config.JHipsterProperties;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(
+    classes = {
+        JHipsterProperties.class,
+        WebConfigurer.class,
+        SecurityConfiguration.class,
+        SecurityJwtConfiguration.class,
+        SecurityMetersService.class,
+        JwtAuthenticationTestUtils.class,
+    }
+)
+public @interface AuthenticationIntegrationTest {
+}
